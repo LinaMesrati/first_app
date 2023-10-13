847bcb27a15f99807d06aed16f435b9ddb3a8635
Lina Mesrati <lina.mesrati@gmail.com>
2023-10-13 15:52:29 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit ,ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation :ViewEncapsulation.ShadowDom
 })
-export class AppComponent {
-  title = 'first-app';
+export class AppComponent implements OnInit{
+  url?:string;
+  show=true;
+  ngOnInit(): void {
+    this.url=this.url1;
+  }
+  title = 'Gestion des livres';
+  jours=['Lundi','Mardi','Mercredi','..']
+  url2="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCGa4e5-x6JueK6vSe5t2XXd_dyFPx-3K-sHdFyE4K&s";
+  url1="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXnspxPwqHmA-DwC6bXBbhMACVu5yaast3ZL70acJA4A&s";
+  changeLogo=()=>
+  {
+    this.url=(this.url===this.url1)?this.url2:this.url1;
+  }
+  showLogo=()=>
+  {
+     this.show =!this.show;
+  }
 }
